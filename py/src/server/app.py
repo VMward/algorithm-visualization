ef0f2affd4467cde937eea2faf86d7c1ab3bfd0f
Benjamin Fan <ben.yan.fan@gmail.com>
2021-04-22 16:38:59 -0600

    added basic graphql

--- py/src/server/app.py
+++ py/src/server/app.py
@@ -1,8 +1,14 @@
+import graphene
 from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return "Hello " + name
+
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
