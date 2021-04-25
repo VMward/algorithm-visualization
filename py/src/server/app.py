5b77d2fe00f2f0316fc952f02831c2d9b812b085
Benjamin Fan <ben.yan.fan@gmail.com>
2021-04-24 22:17:13 -0600

    working on graphql

--- py/src/server/app.py
+++ py/src/server/app.py
@@ -5,6 +5,7 @@ from starlette.graphql import GraphQLApp
 from . import config
 from fastapi.logger import logger
 import logging
+from ..algorithms import strings
 
 gunicorn_logger = logging.getLogger('gunicorn.error')
 logger.handlers = gunicorn_logger.handlers
@@ -16,8 +17,12 @@ def get_settings():
 
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stranger"))
-    def resolve_hello(self, info, name):
+    def resolve_hello(self, name):
         return "Hello " + name
+
+    isAnagram = graphene.String(s1=graphene.String(), s2=graphene.String())
+    def test_anagram(self, s1, s2):
+        return strings.isAnagram(s1, s2)
 
 
 app = FastAPI()
